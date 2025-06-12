c9c1e588849fa9e2db697cf614d96c368e48445a
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-06-12 12:29:05 +0600

    1:31

--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -1,28 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import morgan from 'morgan'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import { createInventory } from "./controllers";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-app.get('/health',(_req,res)=>{
-    res.status(200).json({status:'UP'});
-})
+app.get("/health", (_req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+// routes
+// app.post("/inventories", createInventory as import("express").RequestHandler);
 
 const port = process.env.PORT || 4002;
-const serviceName = process.env.SERVICE_NAME || 'Inventory-Service';
+const serviceName = process.env.SERVICE_NAME || "Inventory-Service";
 
 //Error handler
-app.use((err,_req,res,_next)=>{
-    console.log(err.stack)
-    res.status(500).json({message:'Internal server error'})
-})
+app.use((err, _req, res, _next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
 
-app.listen(port,()=>{
-    console.log(`${serviceName} is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`${serviceName} is running on port ${port}`);
+});
