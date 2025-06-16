ef502c2e59fee9d227de331cfca1fc8fe172df6c
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-06-16 20:22:36 +0600

    2:39:31

---
+++ services/order/src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import { checkout, getOrderById, getOrders } from "./controllers";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+// app.use((req, res, next) => {
+//   const allowedOrigins = ["http://localhost:8001", "http://127.0.0.1:8001"];
+//   const origin = req.headers.origin || "";
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//     next();
+//   } else {
+//     res.status(403).json({ message: "Forbidden" });
+//   }
+// });
+
+// routes
+app.post("/orders/checkout", checkout as any);
+app.post("/orders/:id", getOrderById as any);
+app.post("/orders", getOrders as any);
+//Error handler
+app.use((err, _req, res, _next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
+const port = process.env.PORT || 4007;
+const serviceName = process.env.SERVICE_NAME || "Order-Service";
+
+app.listen(port, () => {
+  console.log(`${serviceName} is running on port ${port}`);
+});
